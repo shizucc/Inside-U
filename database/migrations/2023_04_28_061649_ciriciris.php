6f8ae33f096668e3564947bb7371121dff1f47a4
Yunic10 <yudithnico99@gmail.com>
2023-04-28 15:23:41 +0700

    tambah _

--- database/migrations/2023_04_28_061649_ciriciris.php
+++ database/migrations/2023_04_28_061649_ciriciris.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('ciriciris', function (Blueprint $table) {
+        Schema::create('ciri_ciris', function (Blueprint $table) {
             $table->id();
             $table->string('ciri');
             $table->string('username');
