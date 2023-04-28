e4dba314f02b0b1fbc202c0df0ed9575d1e15f78
shizucc <syifanaalfi@gmail.com>
2023-04-28 11:04:03 +0700

    modify model

--- app/Models/CiriCiri.php
+++ app/Models/CiriCiri.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class CiriCiri extends Model
 {
     use HasFactory;
+    protected $guarded = ['id'];
 }
--- app/Models/DaftarPertanyaan.php
+++ app/Models/DaftarPertanyaan.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class DaftarPertanyaan extends Model
 {
     use HasFactory;
+    protected $guarded = ['id'];
 }
