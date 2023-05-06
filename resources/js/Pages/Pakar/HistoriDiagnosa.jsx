b85d1ee94e341e38359198c5def8ad6c590f36ef
adotrdot <a.aryanta.s@gmail.com>
2023-05-06 13:43:07 +0700

    Done hasil diagnosa kasaran

--- resources/js/Pages/Pakar/HistoriDiagnosa.jsx
+++ resources/js/Pages/Pakar/HistoriDiagnosa.jsx
@@ -1,23 +1,34 @@
 export default function HistoriDiagnosa(props){
     let historis = props.historis;
     historis = historis.filter(histori => histori.kepribadians.length != 0);
-    console.log(historis);
     function hello() {
         <h2>Cek</h2>
     }
     return(
         <>
-        <h1>Hello World</h1>
         {historis ? historis.map(histori => {
             let n = histori.kepribadians.length/4;
             let kepribadians = [];
             for (let i = 0; i < n; i++) {
                 let tmp = histori.kepribadians.slice(0+(i*4), 4+(i*4));
-                let max_kepribadian = histori.reduce((previous, current) => {
-                    current.kepribadians.pivot.persentase > previous.kepribadians.pivot.persentase
-                    ? current : previous });
+                let max_kepribadian = tmp.reduce(
+                    (prev, curr) => prev.pivot.persentase > curr.pivot.persentase ? prev : curr);
                 kepribadians.push(max_kepribadian);
             }
+            return(
+            <>
+                {kepribadians.map(kepribadian => {
+                    console.log(kepribadian);
+                    return(
+                        <>
+                            <h1>Nama: {histori.username}</h1>
+                            <p>Waktu diagnosa: {kepribadian.pivot.waktu_diagnosa}</p>
+                            <p>Hasil: {kepribadian.jenis_kepribadian}</p>
+                        </>
+                    )
+                })}
+            </>
+            )
         }) : null
     }
         </>
