9b4c61a2a8a36d39ef57a70f777310431fe9bca9
adotrdot <a.aryanta.s@gmail.com>
2023-05-07 15:25:36 +0700

    Removes getting monthly_users from DB

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -12,10 +12,6 @@ class AdminController extends Controller
     public function index() {
         $data = [
             'users'=> User::with('kepribadians')->orderBy('id', 'asc')->get(),
-            'monthly_users' => User::select(DB::raw('COUNT(id) as total'))
-                                ->groupBy(DB::raw('MONTH(created_at)'))
-                                ->orderBy(DB::raw('MONTH(created_at)'))
-                                ->get()
         ];
         return Inertia::render('Admin/Index', $data);
     }
