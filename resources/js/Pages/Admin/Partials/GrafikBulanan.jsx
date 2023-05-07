8767c2bdd699de4b46b3a1439d597926efcfed16
adotrdot <a.aryanta.s@gmail.com>
2023-05-07 15:38:11 +0700

    Tambah warna pada grafik bulanan

--- resources/js/Pages/Admin/Partials/GrafikBulanan.jsx
+++ resources/js/Pages/Admin/Partials/GrafikBulanan.jsx
@@ -48,7 +48,20 @@ export default function GrafikBulanan(props) {
         datasets: [{
             label: props.label,
             data: props.data,
-            backgroundColor: 'rgba(255, 99, 132, 0.5)',
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.5)',
+                'rgba(255, 159, 64, 0.5)',
+                'rgba(255, 205, 86, 0.5)',
+                'rgba(75, 192, 192, 0.5)',
+                'rgba(54, 162, 235, 0.5)',
+                'rgba(20, 100, 250, 0.5)',
+                'rgba(153, 102, 255, 0.5)',
+                'rgba(201, 203, 207, 0.5)',
+                'rgba(255, 99, 132, 0.5)',
+                'rgba(255, 159, 64, 0.5)',
+                'rgba(255, 205, 86, 0.5)',
+                'rgba(75, 192, 192, 0.5)',
+            ],
         },],
     };
     return(
