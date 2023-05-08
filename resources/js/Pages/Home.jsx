dacb4157f2b9e2ccb5b60b51819fa55f56be07ec
adotrdot <a.aryanta.s@gmail.com>
2023-05-08 14:49:02 +0700

    Mockup footer

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,9 +1,13 @@
 import Navbar from "@/Components/Navbar";
+import Footer from "@/Components/Footer";
 
 export default function Home(){
     return (
-        <Navbar>
-            <h1>Halo Dunia</h1>
-        </Navbar>
+        <>
+            <Navbar>
+                <h1>Halo Dunia</h1>
+            </Navbar>
+            <Footer />
+        </>
     );
 }
