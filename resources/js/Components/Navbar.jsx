c2f506f15c8b424bdbc69a2100bc695675371e8c
adotrdot <a.aryanta.s@gmail.com>
2023-05-27 01:32:19 +0700

    Add mekanisme guest dan authenticated navigation bar

--- resources/js/Components/Navbar.jsx
+++ resources/js/Components/Navbar.jsx
@@ -6,10 +6,145 @@ import ResponsiveNavLink from '@/Components/ResponsiveNavLink';
 import Footer from '@/Components/Footer';
 import { Head, Link } from '@inertiajs/react';
 
+
 export default function Navbar({ user, children }) {
     const [showingNavigationDropdown, setShowingNavigationDropdown] = useState(false);
 
-    return (
+    function setDropdown() {
+        setShowingNavigationDropdown((previous_state) => !previous_state);
+    }
+
+    if (user == null) {
+        return(
+            <GuestBar children={children} showingNavigationDropdown={showingNavigationDropdown} setDropdown={setDropdown}/>
+        );
+    } else {
+        return(
+            <AuthenticatedBar user={user} children={children} showingNavigationDropdown={showingNavigationDropdown} setDropdown={setDropdown} />
+        );
+    }
+}
+
+function GuestBar({ children, showingNavigationDropdown, setDropdown }) {
+    return(
+        <>
+        <Head>
+            <style>{'body { background-color: rgba(190,200,250,0.5); }'}</style>
+        </Head>
+        <div className="min-h-screen">
+            <nav className="bg-white border-b border-gray-100">
+                <div className="max-w-7xl mx-auto px-10 sm:px-20 lg:px-40">
+                    <div className="flex justify-between h-16">
+                        <div className="flex">
+                            <div className="shrink-0 flex items-center">
+                                <Link href="/">
+                                    <ApplicationLogo />
+                                </Link>
+                            </div>
+
+
+                        </div>
+
+                        <div className="hidden sm:flex sm:items-center sm:ml-6">
+                            <div className="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                                <NavLink href={route('dashboard')} active={route().current('dashboard')}>
+                                    Tentang Kami
+                                </NavLink>
+                            </div>
+                            <div className="ml-3 relative">
+                                <Dropdown>
+                                    <Dropdown.Trigger>
+                                        <span className="inline-flex rounded-md">
+                                            <button
+                                                type="button"
+                                                className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
+                                            >
+                                                Guest
+
+                                                <svg
+                                                    className="ml-2 -mr-0.5 h-4 w-4"
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    viewBox="0 0 20 20"
+                                                    fill="currentColor"
+                                                >
+                                                    <path
+                                                        fillRule="evenodd"
+                                                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                        clipRule="evenodd"
+                                                    />
+                                                </svg>
+                                            </button>
+                                        </span>
+                                    </Dropdown.Trigger>
+
+                                    <Dropdown.Content>
+                                        <Dropdown.Link href={route('profile.edit')}>Profile</Dropdown.Link>
+                                        <Dropdown.Link href={route('logout')} method="post" as="button">
+                                            Log Out
+                                        </Dropdown.Link>
+                                    </Dropdown.Content>
+                                </Dropdown>
+                            </div>
+                        </div>
+
+                        <div className="-mr-2 flex items-center sm:hidden">
+                            <button
+                                onClick={setDropdown}
+                                className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
+                            >
+                                <svg className="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                                    <path
+                                        className={!showingNavigationDropdown ? 'inline-flex' : 'hidden'}
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        d="M4 6h16M4 12h16M4 18h16"
+                                    />
+                                    <path
+                                        className={showingNavigationDropdown ? 'inline-flex' : 'hidden'}
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        d="M6 18L18 6M6 6l12 12"
+                                    />
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={(showingNavigationDropdown ? 'block' : 'hidden') + ' sm:hidden'}>
+                    <div className="pt-2 pb-3 space-y-1">
+                        <ResponsiveNavLink href={route('dashboard')} active={route().current('dashboard')}>
+                            Tentang Kami
+                        </ResponsiveNavLink>
+                    </div>
+
+                    <div className="pt-4 pb-1 border-t border-gray-200">
+                        <div className="px-4">
+                            <div className="font-medium text-base text-gray-800">Guest</div>
+                        </div>
+
+                        <div className="mt-3 space-y-1">
+                            <ResponsiveNavLink href={route('profile.edit')}>Profile</ResponsiveNavLink>
+                            <ResponsiveNavLink method="post" href={route('logout')} as="button">
+                                Log Out
+                            </ResponsiveNavLink>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+
+            <main>{children}</main>
+        </div>
+        <Footer />
+        </>
+
+    );
+}
+
+function AuthenticatedBar({ user, children, showingNavigationDropdown, setDropdown }) {
+    return(
         <>
         <Head>
             <style>{'body { background-color: rgba(190,200,250,0.5); }'}</style>
@@ -72,7 +207,7 @@ export default function Navbar({ user, children }) {
 
                         <div className="-mr-2 flex items-center sm:hidden">
                             <button
-                                onClick={() => setShowingNavigationDropdown((previousState) => !previousState)}
+                                onClick={setDropdown}
                                 className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
                             >
                                 <svg className="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
@@ -105,8 +240,7 @@ export default function Navbar({ user, children }) {
 
                     <div className="pt-4 pb-1 border-t border-gray-200">
                         <div className="px-4">
-                            <div className="font-medium text-base text-gray-800">aaaaa</div>
-                            <div className="font-medium text-sm text-gray-500">aaaaaa</div>
+                            <div className="font-medium text-base text-gray-800">{user.username}</div>
                         </div>
 
                         <div className="mt-3 space-y-1">
